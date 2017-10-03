28dbd057c2022a7fe7e8b326abf68bdcb354a822
Kyle Hoernlein <khoernlein90@gmail.com>
2017-10-03 09:42:04 -0400

    added log

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var keys = require("./keys.js");
+var fs = require("fs");
 
 // console.log(keys);
 
@@ -47,10 +48,28 @@ function spotifyThis(input, songOrMovie) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
+            console.log("")
+            console.log("")
+            console.log("-----------------------------------------------------------------------------------------------------------------");
             console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name) // BAND NAME
+            console.log("-----------------------------------------------------------------------------------------------------------------");
             console.log("Song Name: " + data.tracks.items[0].name) // SONG NAME
+            console.log("-----------------------------------------------------------------------------------------------------------------");
             console.log("Song URL: " + data.tracks.items[0].external_urls.spotify) // SONG URL
+            console.log("-----------------------------------------------------------------------------------------------------------------");
             console.log("Song Album: " + data.tracks.items[0].album.name) //SONG ALBUM
+            console.log("-----------------------------------------------------------------------------------------------------------------");
+            console.log("")
+            console.log("")
+            fs.appendFile("log.txt", "Artist(s): " + data.tracks.items[0].album.artists[0].name + "\n" +
+                "Song Name: " + data.tracks.items[0].name + "\n" +
+                "Song URL: " + data.tracks.items[0].external_urls.spotify + "\n" +
+                "Song Album: " + data.tracks.items[0].album.name + "\n" + "\n",
+                function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                })
         });
     } else if (input === "spotify-this-song") {
         getSpotify.search({
@@ -61,10 +80,28 @@ function spotifyThis(input, songOrMovie) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
+            console.log("")
+            console.log("")
+            console.log("-----------------------------------------------------------------------------------------------------------------");
             console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name) // BAND NAME
+            console.log("-----------------------------------------------------------------------------------------------------------------");
             console.log("Song Name: " + data.tracks.items[0].name) // SONG NAME
+            console.log("-----------------------------------------------------------------------------------------------------------------");
             console.log("Song URL: " + data.tracks.items[0].external_urls.spotify) // SONG URL
+            console.log("-----------------------------------------------------------------------------------------------------------------");
             console.log("Song Album: " + data.tracks.items[0].album.name) //SONG ALBUM
+            console.log("-----------------------------------------------------------------------------------------------------------------");
+            console.log("")
+            console.log("")
+            fs.appendFile("log.txt", "Artist(s): " + data.tracks.items[0].album.artists[0].name + "\n" +
+                "Song Name: " + data.tracks.items[0].name + "\n" +
+                "Song URL: " + data.tracks.items[0].external_urls.spotify + "\n" +
+                "Song Album: " + data.tracks.items[0].album.name + "\n" + "\n",
+                function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                })
         });
     }
 }
@@ -82,14 +119,40 @@ if ((input === "movie-this") && (!songOrMovie)) {
         if (err) {
             return console.log(err);
         }
+        console.log("")
+        console.log("")
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Movie name: " + JSON.parse(body).Title);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Release Date: " + JSON.parse(body).Year);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("IMDb Rating: " + JSON.parse(body).imdbRating);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Country: " + JSON.parse(body).Country);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Language: " + JSON.parse(body).Language);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Movie Plot: " + JSON.parse(body).Plot);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
+        console.log("")
+        console.log("")
+        fs.appendFile("log.txt", "Movie name: " + JSON.parse(body).Title + "\n" +
+            "Release Date: " + JSON.parse(body).Year + "\n" +
+            "IMDb Rating: " + JSON.parse(body).imdbRating + "\n" +
+            "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n" +
+            "Country: " + JSON.parse(body).Country + "\n" +
+            "Language: " + JSON.parse(body).Language + "\n" +
+            "Movie Plot: " + JSON.parse(body).Plot + "\n" +
+            "Actors: " + JSON.parse(body).Actors + "\n" + "\n",
+            function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            })
     })
 } else if (input === "movie-this") {
 
@@ -99,18 +162,44 @@ if ((input === "movie-this") && (!songOrMovie)) {
         if (err) {
             return console.log(err);
         }
+        console.log("")
+        console.log("")
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Movie name: " + JSON.parse(body).Title);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Release Date: " + JSON.parse(body).Year);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("IMDb Rating: " + JSON.parse(body).imdbRating);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Country: " + JSON.parse(body).Country);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Language: " + JSON.parse(body).Language);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Movie Plot: " + JSON.parse(body).Plot);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
         console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("-----------------------------------------------------------------------------------------------------------------");
+        console.log("")
+        console.log("")
+        fs.appendFile("log.txt", "Movie name: " + JSON.parse(body).Title + "\n" +
+            "Release Date: " + JSON.parse(body).Year + "\n" +
+            "IMDb Rating: " + JSON.parse(body).imdbRating + "\n" +
+            "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n" +
+            "Country: " + JSON.parse(body).Country + "\n" +
+            "Language: " + JSON.parse(body).Language + "\n" +
+            "Movie Plot: " + JSON.parse(body).Plot + "\n" +
+            "Actors: " + JSON.parse(body).Actors + "\n" + "\n",
+            function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            })
+
     })
 }
 /////////////////////     ----------RANDOM.TXT---------     ////////////////////////////////////////////////////////////////////////////////////////////
-var fs = require("fs");
 
 if (input === "do-what-it-says") {
     fs.readFile("random.txt", "utf8", function(err, data) {
@@ -128,7 +217,6 @@ if (input === "do-what-it-says") {
 var Twitter = require('twitter');
 var twitKeys = keys.twitterKeys;
 var getTwitter = new Twitter(twitKeys);
-// console.log(twister);
 
 if (input === "my-tweets") {
     var params = {
@@ -145,6 +233,19 @@ if (input === "my-tweets") {
                 counter++;
                 console.log("-----------------------------------------------------------------------------------------------------------------");
                 console.log("Tweet " + counter + ": " + tweets[i].text);
+
+                fs.appendFile("log.txt", "Tweet " + counter + ": " + tweets[i].text + "\n", function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                })
+            }
+            if (tweets[tweets.length - 1]) {
+                fs.appendFile("log.txt", "" + "\n", function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                })
             }
         }
     });
